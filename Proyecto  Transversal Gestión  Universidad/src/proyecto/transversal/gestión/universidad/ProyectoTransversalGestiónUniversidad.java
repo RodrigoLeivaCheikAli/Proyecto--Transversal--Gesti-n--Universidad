aebc628f3d96d9f77e7cd7e8b34af2737de5c47b
FJI42 <florenciajimenaifran@gmail.com>
2023-09-08 11:48:47 -0300

    Conexion

--- Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/ProyectoTransversalGestiónUniversidad.java
+++ Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/ProyectoTransversalGestiónUniversidad.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.transversal.gestión.universidad;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+import proyecto.transversal.gestión.universidad.accesoADatos.Conexion;
+
 public class ProyectoTransversalGestiónUniversidad {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection con= Conexion.getConexion();
+        
     }
     
 }
--- Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/Conexion.java
+++ Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/Conexion.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package proyecto.transversal.gestión.universidad.accesoADatos;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author usuario
  */
 public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidadulp";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection; 
+    
+    private Conexion(){}
+    
+    public static Connection getConexion(){
+        if(connection == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection= DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                
+                
+                        
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error al cargar los driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error al conectarse a la BD");
+            }
+            
+        }
+        return connection; 
+    }
+    
     
 }
