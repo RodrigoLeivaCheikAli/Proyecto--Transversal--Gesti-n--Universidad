e50d2f47df0619b582b5cee7bbc5843f15012427
RodrigoLeivaCheikAli <141652879+RodrigoLeivaCheikAli@users.noreply.github.com>
2023-09-08 11:02:40 -0300

    Create Alumno.java

---
+++ Proyecto  Transversal Gestión  Universidad/src/Grupo33_universidad_Entidades/newpackage/Alumno.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grupo33_universidad_Entidades.newpackage;
+
+/**
+ *
+ * @author User
+ */
+public class Alumno {
+    
+}
