1eb12d8f2342a928264eb4975d11823d02cb1b7f
FJI42 <florenciajimenaifran@gmail.com>
2023-09-11 11:36:56 -0300

    Inicio InscripcionData

--- Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/IncripcionData.java
+++ Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/IncripcionData.java
@@ -5,10 +5,50 @@
  */
 package proyecto.transversal.gestión.universidad.accesoADatos;
 
+import Grupo33_universidad_Entidades.newpackage.Alumno;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import Grupo33_universidad_Entidades.newpackage.Inscripcion;
+
 /**
  *
  * @author usuario
  */
 public class IncripcionData {
+        private Connection con= null;
     
+    public IncripcionData(){       
+        con= Conexion.getConexion();       
+    }
+    
+    private MateriaData matData; 
+    private AlumnoData aluData; 
+    
+    public void GuardarInscripcion(Inscripcion inscripcion){
+        String sql="INSERT INTO inscripcion(nota, id_alumno,id_materia) VALUES (?,?,?)";
+        
+        try {
+            PreparedStatement ps= con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,inscripcion.getNota());
+            ps.setInt(2,inscripcion.getId_alumno());
+            ps.setInt(3,inscripcion.getId_materia());
+           
+            ps.executeUpdate();
+            
+            ResultSet rs= ps.getGeneratedKeys();
+            if(rs.next()){
+                JOptionPane.showMessageDialog(null, "Inscripcion guardada");
+            }
+            
+            ps.close(); 
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion");
+        }
+                   
+    }
 }
