ae065b402e84256b36500534b9107f75687abbb6
RodrigoLeivaCheikAli <141652879+RodrigoLeivaCheikAli@users.noreply.github.com>
2023-09-13 11:05:07 -0300

    actualizacion

--- Proyecto  Transversal Gestión  Universidad/src/Grupo33_Universidad/Vistas/Menu.java
+++ Proyecto  Transversal Gestión  Universidad/src/Grupo33_Universidad/Vistas/Menu.java
@@ -27,10 +27,21 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu2 = new javax.swing.JMenu();
         jDesktopPane1 = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu7 = new javax.swing.JMenu();
+        jMenu9 = new javax.swing.JMenu();
+        jMenu8 = new javax.swing.JMenu();
+
+        jMenu2.setText("jMenu2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,12 +56,44 @@ public class Menu extends javax.swing.JFrame {
             .addGap(0, 732, Short.MAX_VALUE)
         );
 
-        jMenu1.setText("File");
+        jMenu1.setText("Alumno");
 
-        jMenu3.setText("jMenu3");
+        jMenu3.setText("Formulario de Alumno");
         jMenu1.add(jMenu3);
 
         jMenuBar1.add(jMenu1);
+
+        jMenu4.setText("Materia");
+
+        jMenu5.setText("Formulario de Materia");
+        jMenu4.add(jMenu5);
+
+        jMenuBar1.add(jMenu4);
+
+        jMenu6.setText("Administracion");
+
+        jMenuItem1.setText("Manejo de Inscripciones");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem1);
+
+        jMenuItem2.setText("Manipulacion de Notas");
+        jMenu6.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu6);
+
+        jMenu7.setText("Consultas");
+
+        jMenu9.setText("Alumos por Materia");
+        jMenu7.add(jMenu9);
+
+        jMenuBar1.add(jMenu7);
+
+        jMenu8.setText("Salir");
+        jMenuBar1.add(jMenu8);
 
         setJMenuBar(jMenuBar1);
 
@@ -67,6 +110,10 @@ public class Menu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,7 +153,16 @@ public class Menu extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
+    private javax.swing.JMenu jMenu9;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
