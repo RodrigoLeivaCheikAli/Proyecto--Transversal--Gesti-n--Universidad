f2d49794440e78b6aa8361677047d6d644e3c2a1
FJI42 <florenciajimenaifran@gmail.com>
2023-09-13 10:59:53 -0300

    Modificacciones

--- Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/IncripcionData.java
+++ Proyecto  Transversal Gestión  Universidad/src/proyecto/transversal/gestión/universidad/accesoADatos/IncripcionData.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.transversal.gestión.universidad.accesoADatos;
 
 import Grupo33_universidad_Entidades.newpackage.Alumno;
@@ -14,33 +10,27 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import Grupo33_universidad_Entidades.newpackage.Inscripcion;
 
-/**
- *
- * @author usuario
- */
+
 public class IncripcionData {
         private Connection con= null;
     
     public IncripcionData(){       
-        con= Conexion.getConexion();       
+        this.con= Conexion.getConexion();       
     }
-    
-    private MateriaData matData; 
-    private AlumnoData aluData; 
-    
-    public void GuardarInscripcion(Inscripcion inscripcion){
+ 
+    public void guardarInscripcion(Inscripcion insc){
         String sql="INSERT INTO inscripcion(nota, id_alumno,id_materia) VALUES (?,?,?)";
         
         try {
             PreparedStatement ps= con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1,inscripcion.getNota());
-            ps.setInt(2,inscripcion.getId_alumno());
-            ps.setInt(3,inscripcion.getId_materia());
-           
+            ps.setDouble(1,insc.getNota());
+            ps.setInt(2,insc.getAlumno().getId_alumno());
+            ps.setInt(3,insc.getMateria().getIdMateria());           
             ps.executeUpdate();
             
             ResultSet rs= ps.getGeneratedKeys();
             if(rs.next()){
+                
                 JOptionPane.showMessageDialog(null, "Inscripcion guardada");
             }
             
