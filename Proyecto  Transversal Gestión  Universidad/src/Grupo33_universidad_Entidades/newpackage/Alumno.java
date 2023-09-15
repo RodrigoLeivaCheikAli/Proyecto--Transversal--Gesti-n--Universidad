a42b3c5d6d995cfe8966bcbafbcd1b32ee47b0b3
FJI42 <florenciajimenaifran@gmail.com>
2023-09-15 12:14:59 -0300

    -

--- Proyecto  Transversal Gestión  Universidad/src/Grupo33_universidad_Entidades/newpackage/Alumno.java
+++ Proyecto  Transversal Gestión  Universidad/src/Grupo33_universidad_Entidades/newpackage/Alumno.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Grupo33_universidad_Entidades.newpackage;
 
-import java.sql.Date;
+
 import java.time.LocalDate;
 
 /**
